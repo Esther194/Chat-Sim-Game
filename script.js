64cd2e90972e0f323afff1cd2be196cb48f2506b
Esther194 <135501784+Esther194@users.noreply.github.com>
2026-03-28 16:49:28 +0800

    Update script.js
    新增按鈕確定能根據不同按鈕觸發不同對話

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-var A = [0, 0, 0, 0, 0, 0];
+var A = [0, 0, 0, 0, 0, 0, 0];
 var B = 0;
+var Lin=[],Yu=[],ren=[],Hayes=[],Dad=[],Mom=[];
 
-function click1(i, value) {
-  A[i] += value;
-
+function click1(n, plus) {
+  var button1 = document.querySelector("button");
+  button1.parentNode.removeChild(button1);
+  var button2 = document.querySelector("button");
+  button2.parentNode.removeChild(button2);
+  
+  A[n] += plus;
+  
+  var container = document.getElementById("footer");
+  
   switch (B) {
     case 0:
       var p1 = [
@@ -11,52 +19,84 @@ function click1(i, value) {
         "設定超過內容高度時出現捲軸0",
         "設定超過內容高度時出現捲軸0"
       ];
-      addp(p1);
+      addp(p1, 5, 1, 4, 1);
+      
+      Lin=Lin.concat(p1);
+      
+      A[0] += 1;
       break;
 
     case 1:
       var p1 = [
-        "設定超過內容高度時出現捲軸0",
         "設定超過內容高度時出現捲軸1",
-        "設定超過內容高度時出現捲軸2"
+        "設定超過內容高度時出現捲軸1",
+        "設定超過內容高度時出現捲軸1"
       ];
-      addp(p1);
+      addp(p1, 0, 2, 0, 1);
+      if (A[0] === 3) {
+        A[2] += 2;
+      } else {
+        A[2] += 1;
+      }
       break;
 
     case 2:
       var p1 = [
-        "設定超過內容高度時出現捲軸1",
         "設定超過內容高度時出現捲軸2",
-        "設定超過內容高度時出現捲軸3"
+        "設定超過內容高度時出現捲軸2",
+        "設定超過內容高度時出現捲軸2"
       ];
-      addp(p1);
+      addp(p1, 0, 1, 0, 0);
+      if (A[0] === 4) {
+        A[5] += 1;
+      }
       break;
 
     case 3:
       var p1 = [
-        "設定超過內容高度時出現捲軸2",
         "設定超過內容高度時出現捲軸3",
-        "設定超過內容高度時出現捲軸4"
+        "設定超過內容高度時出現捲軸3",
+        "設定超過內容高度時出現捲軸3"
       ];
-      addp(p1);
+      addp(p1, 0, 2, 0, 1);
       break;
-      case 4:
+      
+    case 4:
       var p1 = [
-        "設定超過內容高度時出現捲軸2",
-        "設定超過內容高度時出現捲軸3",
+        "設定超過內容高度時出現捲軸4",
+        "設定超過內容高度時出現捲軸4",
         "設定超過內容高度時出現捲軸4"
       ];
-      addp(p1);
+      addp(p1, 6, 0, 6, 1);
+      break;
+   
+      case 5:
+      if (A[6] == 0) {
+            var p1 = [
+              "設定超過內容高度時出現捲軸",
+              "設定超過內容高度時出現捲軸",
+              "設定超過內容高度時出現捲軸"
+            ];
+            addp(p1, 7);
+          } else {
+            var p1 = [
+              "設定超過內容高度時出現捲軸1",
+              "設定超過內容高度時出現捲軸1",
+              "設定超過內容高度時出現捲軸1"
+            ];
+            addp(p1, 7);
+          }
       break;
   }
 
-  B += 1;
+  
 }
 
-function addp(p1) {
+function addp(p1, n1, plus1 = 0, n2 = 0, plus2 = 0, delay = 500) {
   var conversation = p1;
-  var b = document.getElementById("content");
-  var delay = 500;
+  var box1 = document.getElementById("content");
+  var box2 = document.getElementById("footer");
+  var delay = delay;
 
   for (var i = 0; i < conversation.length; i++) {
     (function(index) {
@@ -68,8 +108,29 @@ function addp(p1) {
         p.appendChild(span);
         p.appendChild(document.createElement("br"));
 
-        b.appendChild(p);
+        box1.appendChild(p);
+
+        if (index === conversation.length - 1 && n1 !== 7) {
+          var button1 = document.createElement("button");
+          button1.innerHTML = "01";
+          button1.onclick = function() {
+            B += 1;
+            click1(n1, plus1);
+          };
+
+          var button2 = document.createElement("button");
+          button2.innerHTML = "02";
+          button2.onclick = function() {
+            B += 2;
+            click1(n2,plus2);
+          };
+
+          box2.appendChild(button1);
+          box2.appendChild(button2);
+        }
+
       }, delay * index);
     })(i);
   }
 }
+
